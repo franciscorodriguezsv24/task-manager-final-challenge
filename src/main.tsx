7ada5d3d056e0cf76a09a22be8f8c9f4e0134c89
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2025-09-09 15:28:14 -0600

    feat: add libraries and create folder with core components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { Layout } from "./Layout.tsx";
+import { Home } from "./page/home/Home.tsx";
+import { MyTask } from "./page/myTask/MyTask.tsx";
+import { User } from "./page/user/User.tsx";
+import { NotFound } from "./page/notFound/NotFound.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "my-task",
+        element: <MyTask />,
+      },
+      {
+        path: "user-info",
+        element: <User />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
