1877ca9898eaf14444911d543936abd2361cd15c
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2025-09-10 17:15:32 -0600

    feat: create sidebar with mobile view and addapted react router

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,3 +1,26 @@
+import styles from "./sidebar.module.scss";
+import Logo from "../../../assets/Logo.svg";
+import { Link } from "react-router";
+import { RiGalleryView2, RiMenuLine, RiAddCircleFill } from "@remixicon/react";
+
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className={styles.sidebarContainer}>
+      <img src={Logo} alt="logo-ravn" className={styles.logoImg} />
+      <div className={styles.linkContainer}>
+        <Link to="/" className={styles.linkElement}>
+          <RiGalleryView2 className={styles.linkIcon} />
+          Dashboard
+        </Link>
+        <button className={styles.buttonMobile}>
+          <RiAddCircleFill className={styles.linkIcon} />
+          Add Project
+        </button>
+        <Link to="my-task" className={styles.linkElement}>
+          <RiMenuLine className={styles.linkIcon} />
+          My Task
+        </Link>
+      </div>
+    </div>
+  );
 };
