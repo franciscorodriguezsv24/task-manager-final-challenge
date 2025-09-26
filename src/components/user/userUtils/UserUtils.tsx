4b5779170159471d3fa008387c2af0b4c1cc0aa1
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2025-09-26 08:34:31 -0600

    feat: dark and light actions

--- src/components/user/userUtils/UserUtils.tsx
+++ src/components/user/userUtils/UserUtils.tsx
@@ -1,12 +1,20 @@
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
-import { RiLogoutBoxRLine, RiUserLine } from "@remixicon/react";
+import {
+  RiLogoutBoxRLine,
+  RiMoonFill,
+  RiSunFill,
+  RiUserLine,
+} from "@remixicon/react";
 import { Avatar } from "../../ui/avatar/Avatar";
 import styles from "./userUtils.module.scss";
 import { Link } from "react-router";
 import { Text } from "../../ui/text/Text";
 import img from "../../../assets/profile.jpg";
+import { useThemeStore } from "../../../store/useThemeManager";
 
 export const UserUtils = () => {
+  const { theme, toggleTheme } = useThemeStore();
+
   return (
     <div>
       <Menu>
@@ -24,6 +32,14 @@ export const UserUtils = () => {
           <MenuItem as="button" className={styles.buttonDropdownUser}>
             <RiLogoutBoxRLine />
             <Text variant="subtitle">Logout</Text>
+          </MenuItem>
+
+          <MenuItem
+            as="button"
+            onClick={toggleTheme}
+            className={styles.buttonDropdownUser}
+          >
+            {theme === "light" ? <RiMoonFill /> : <RiSunFill />} Switch
           </MenuItem>
         </MenuItems>
       </Menu>
